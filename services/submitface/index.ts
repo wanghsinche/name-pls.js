4506b9bda89b98d9114a12a57a9af72b622ba7e3
author <author@fourf>
2022-01-15 20:46:16 +0800

    fix: oauth

--- services/submitface/index.ts
+++ services/submitface/index.ts
@@ -2,8 +2,8 @@ import { Octokit } from '@octokit/rest';
 import { createOAuthAppAuth } from '@octokit/auth-oauth-app';
 
 const info = {
-    clientId: "Iv1.7500a00d02d745c7",
-    clientSecret: "a10536e16c1a9a0d1d6cf5c110a12e8a13ea1409",
+    clientId: "d2ebd1689021e2ee92aa",
+    clientSecret: "4ed8688f66db2d179eae54c1bce0576ba9a0e713",
 
     // clientId: "95353c42e0068b5dbbbf",
     // clientSecret: "f7ce87de2b6d7941c407cb6d9a51765559c7e5f2",
