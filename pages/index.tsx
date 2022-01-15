b235a6d55220ecdcbcf98abf8bbdc5280ac7ffca
author <author@fourf>
2022-01-15 17:10:49 +0800

    fix: UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
 import Cropper from 'react-easy-crop'
 import React, { useState, useCallback, useMemo, useRef, createRef } from 'react'
 import { Area } from 'react-easy-crop/types'
@@ -13,6 +12,7 @@ import Image from 'next/image'
 import { Footer } from '@/components/footer'
 import { Nav } from '@/components/nav'
 import { info } from '@/components/info'
+import { Header } from '@/components/header'
 
 const api = '/api/recog';
 
@@ -58,15 +58,7 @@ const Home: NextPage = () => {
 
   return (
     <div >
-      <Head>
-        <title>Name PLS</title>
-        <meta name="description" content="Japanese Adult Video Star Face Recognition, Search Actress By Image, Powered By Deep Learning" />
-        <meta name="keywords" content="Face Recognition, Search Star By Image, Deep Learning, AI, JAV"></meta>
-        <link rel="icon" href="/favicon.ico" />
-        <link href="https://cdn.jsdelivr.net/npm/daisyui@1.22.2/dist/full.css" rel="stylesheet" type="text/css" />
-        <script src="https://cdn.tailwindcss.com/3.0.11"></script>
-      </Head>
-
+      <Header/>
       <Nav />
       
       <main >
