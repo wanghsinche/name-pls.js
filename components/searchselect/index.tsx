a88089f4e9525b9a3ac60e89e83c1cadac2cb537
author <author@fourf>
2022-01-15 13:59:48 +0800

    fix: update

--- components/searchselect/index.tsx
+++ components/searchselect/index.tsx
@@ -15,7 +15,7 @@ export const SearchSelect: React.FC<IProps> = (p) => {
     const [show, setShow] = useState(false);
     const realV: string = v || p.value as string;
 
-    const showOpt = useMemo(() => word ? p.options.filter(el => (el.value + el.label).toLowerCase().includes(word)).slice(0, 10) : p.options.slice(0,10), [word, p.options])
+    const showOpt = useMemo(() => word ? p.options.filter(el => (el.value + el.label).toLowerCase().includes(word)).slice(0, 5) : p.options.slice(0,5), [word, p.options])
 
     const cOnChange = useCallback((v: string) => {
         if (p.onChange) {
@@ -32,11 +32,11 @@ export const SearchSelect: React.FC<IProps> = (p) => {
         inputRef.current?.focus();
     }, [show])
 
-    return <div className="form-control" style={{ position: 'relative' }}>
+    return <div className="form-control w-1/2" style={{ position: 'relative' }}>
         <div className="fixed left-0 top-0 w-full h-full z-0" style={{ display: show ? 'block' : 'none' }} onClick={() => { setShow(false); }}></div>
-        <input className="input input-bordered" style={{ display: !show ? 'block' : 'none' }} onClick={() => { setShow(true); }} value={realV} placeholder="actress name"></input>
-        <div className="z-10 dropdown" style={{ display: show ? 'block' : 'none' }} >
-            <input ref={inputRef} value={word} onChange={e => setWord(e.target.value.toLowerCase())} type="text" placeholder="actress name" className="input input-bordered" />
+        <input className="input input-bordered w-full" style={{ display: !show ? 'block' : 'none' }} onClick={() => { setShow(true); }} value={realV} placeholder="actress name"></input>
+        <div className="z-10 dropdown w-full" style={{ display: show ? 'block' : 'none' }} >
+            <input ref={inputRef} value={word} onChange={e => setWord(e.target.value.toLowerCase())} type="text" placeholder="actress name" className="input input-bordered w-full" />
             <ul tabIndex={0} className="shadow bg-base-100 menu dropdown-content w-full mt-2">
                 {showOpt.length === 0 && <li><a>No Data</a></li>}
                 {showOpt.map(el => <li key={el.value}><a onClick={() => { cOnChange(el.value); }}>{el.label}</a></li>)}
--- pages/api/actresslist.ts
+++ pages/api/actresslist.ts
@@ -1,10 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
-const classes:string[] = require('@/services/recog/classes');
+const classes:{id:string;cast:string;alias:string;}[] = require('@/services/recog/xslist-cast');
 
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<{label:string;value:string}[]>
 ){
-    res.json(classes.map(el=>({label:el, value:el})));
+    res.json(classes.map(el=>({label:el.alias.replace(/\/Age \d+/,''), value:`${el.id}/${el.cast.replaceAll(' ','_')}`})));
 }
