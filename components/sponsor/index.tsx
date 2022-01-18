109fc48054b0511b212f65f84654be8634bd73e3
author <author@fourf>
2022-01-18 12:14:59 +0800

    fix: eslint

--- components/sponsor/index.tsx
+++ components/sponsor/index.tsx
@@ -6,15 +6,15 @@ export const Sponsor = () => {
     <div id="about-modal" className="modal">
       <div className="modal-box">
         <div className="flex justify-around">
-          <Image src="/android-chrome-512x512.png" height={140} width={140} title="icon" />
+          <Image src="/android-chrome-512x512.png" height={140} width={140} title="icon" alt="name-pls.js" />
         </div>
         <h2 className="text-neutral text-center my-4">
           name-pls.js 
         </h2>
         <p className="text-neutral my-4">
-          A Face Recognize Solution written in NodeJS. Using SOTA model optimized with eastern asian face. Running on microsoft's ONNX Runntime.</p>
+          A Face Recognize Solution written in NodeJS. Using SOTA model optimized with eastern asian face. Running on microsoft&apos;s ONNX Runntime.</p>
         <p className="text-neutral my-8">
-          Star it on <a href="https://github.com/wanghsinche/name-pls.js" target="_blank" className="link">GITHUB</a>  ❤️ </p>
+          Star it on <a href="https://github.com/wanghsinche/name-pls.js" target="_blank" rel="noreferrer" className="link">GITHUB</a>  ❤️ </p>
         <div className="modal-action">
           <a href="#" className="btn">Close</a>
         </div>
